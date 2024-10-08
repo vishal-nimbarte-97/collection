369ae6893b2d6dd93049a2fe5786961a7fb6207f
Vishal Nimbarte <vishalnimbarte097@gmail.com>
2024-10-08 11:21:32 +0530

    Minor Changes In Port

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
